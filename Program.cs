531f0133c79cdad63e9cbb8ad6c4da802444eeed
Amanda Valente <amandavalente.santos@gmail.com>
2024-01-06 18:11:58 -0300

    desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone Nokia = new Nokia(numero: "2345", modelo: "tipo1", imei: "abcs", memoria: 64);
+Nokia.Ligar();
+Nokia.InstalarAplicativo("Spotfy");
+Nokia.ReceberLigacao();
+
+Smartphone iphone = new Iphone(numero: "56748", modelo: "tipo3", imei: "gdhgf", memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+
+
